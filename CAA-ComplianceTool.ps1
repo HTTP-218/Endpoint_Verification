e725dc6b947ba78e8946a21612277283ba0a1e0e
2mmkolibri <2mmkolibri@proton.me>
2025-07-29 11:29:23 +0100

    Upload CAA Compliance files

---
+++ CAA-ComplianceTool.ps1
@@ -0,0 +1,295 @@
+##########################################################################
+#                                                                        #
+#                    		      CAA-ComplianceTool.ps1                    		      #
+#                                                                        #
+######################################################################################################
+
+#====================================================================================================#
+#                                           [ Variables ]                                            #
+#====================================================================================================#
+
+Add-Type -AssemblyName System.Windows.Forms
+$ErrorActionPreference = "Stop"
+$LogFilePath = "C:\Windows\Temp\CAA-Compliance.log"
+$JSONPath = "C:\Windows\Temp\caa.json"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1D-LoZOFrcXmFEulDjfywONZ2O12d98ty" -OutFile $JSONPath
+$Variables = Get-Content -Raw -Path $JSONPath | ConvertFrom-Json
+$EVHelperPath = "C:\Windows\Temp\EndpointVerification_admin.msi"
+$EVHelperURL = 'https://dl.google.com/dl/secureconnect/install/win/EndpointVerification_admin.msi'
+$Summary = @()
+
+#====================================================================================================#
+#                                           [ Functions ]                                            #
+#====================================================================================================#
+
+#region Functions
+function Write-Log {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Level,    
+        [string]$Message
+    )
+    $Timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+    $LogEntry = "$Timestamp - [$Level] $Message"
+    $LogEntry | Out-File -FilePath $LogFilePath -Append -Encoding Unicode
+}
+
+function Show-MessageBox {
+    param (
+        [string]$Message,
+        [string]$Title = "Notice",
+        [ValidateSet("Information", "Warning", "Error", "Question")]
+        [string]$Icon = "Information",
+        [System.Windows.Forms.MessageBoxButtons]$Buttons = [System.Windows.Forms.MessageBoxButtons]::OK
+    )
+    return [System.Windows.Forms.MessageBox]::Show($Message, $Title, $Buttons, [System.Windows.Forms.MessageBoxIcon]::$Icon)
+}
+
+# Wrapper for different log message types
+function Write-Message {
+    param (
+        [string]$Message,
+        [ValidateSet("INFO", "NOTICE", "WARN", "ERROR")] 
+        [string]$Level = "INFO",
+        [bool]$Console = $true,
+        [bool]$Log = $true,
+        [bool]$Dialogue = $false,
+        [string]$DialogueTitle = "CAA Compliance Tool"
+    )
+
+    if ($Console) { Write-Host "[$Level] $Message" }
+    if ($Log) { Write-Log -Level $Level -Message $Message }
+    if ($Dialogue) {
+        $icon = switch ($Level) {
+            "INFO"   { "Information" }
+            "NOTICE" { "Information" }
+            "WARN"   { "Warning" }
+            "ERROR"  { "Error" }
+        }
+        Show-MessageBox -Message $Message -Title $DialogueTitle -Icon $icon
+    }
+}
+
+function Get-ConsoleUser {
+    $session = (Get-CimInstance Win32_ComputerSystem).UserName
+    if ($session) {
+        return $session.Split('\')[-1]
+    } else {
+        Write-Message -Message "No user currently logged into console session" -Level "WARN"
+        return $null
+    }
+}
+
+#endregion
+
+Set-Content -Path $LogFilePath -Encoding Unicode -Value "
+##########################################################################
+#                                                                        #
+#                         CAA-ComplianceTool.ps1                         #
+#                                                                        #
+##########################################################################
+"
+
+#====================================================================================================#
+#                                      [ Windows Build Check ]                                       #
+#====================================================================================================#
+$WindowsBuild = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").CurrentBuildNumber
+$DisplayVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion
+
+$MatchingRequirement = $Variables.BuildRequirements |
+Where-Object { $WindowsBuild -ge $_.MinBuild -and ($_.MaxBuild -eq $null -or $WindowsBuild -le $_.MaxBuild) } |
+Sort-Object MinBuild -Descending |
+Select-Object -First 1
+
+if ($null -eq $MatchingRequirement) {
+    $Message = "Windows build $($DisplayVersion) is not compliant"
+    Write-Message -Message $Message -Level "WARN"
+    $Summary += $Message
+} 
+else {
+    Write-Message -Message "Windows is compliant with $($MatchingRequirement.Label) (Build $WindowsBuild)" -Level "INFO"
+}
+
+#====================================================================================================#
+#                                      [ Chrome Version Check ]                                      #
+#====================================================================================================#
+$ChromeVersion = (Get-Package | Where-Object { $_.Name -like "*Google Chrome*" }).Version 
+
+if ($ChromeVersion -lt $Variables.ChromeVersion) {
+    Write-Message -Message "Chrome is not compliant" -Level "WARN"
+    $Summary += "Chrome version $ChromeVersion is below the minimum requirement"
+}
+else {
+    Write-Message -Message "Chrome is compliant with version $($ChromeVersion)" -Level "INFO"
+}
+
+#====================================================================================================#
+#                             [ Endpoint Verification Extension Check ]                              #
+#====================================================================================================#
+$CurrentUser = Get-ConsoleUser
+$ChromeProfiles = Get-ChildItem -Path "C:\Users\$CurrentUser\AppData\Local\Google\Chrome\User Data" -Directory | Where-Object { $_.Name -eq "Default" -or $_.Name -match "^Profile \d+$" }
+$EVExtension = @()
+
+foreach ($ChromeProfile in $ChromeProfiles) {
+    $ExtensionsFolder = Get-ChildItem -Path $ChromeProfile.FullName | Where-Object { $_.Name -eq "Extensions" }
+    if ($ExtensionsFolder) {
+        $ExtensionsPath = Get-ChildItem -Path $ExtensionsFolder.FullName | Where-Object { $_.Name -eq $Variables.ExtensionID}
+        if ($ExtensionsPath){
+            $EVExtension += $ExtensionsPath
+        }
+    }
+}
+
+if ($EVExtension) {
+    Write-Message -Message "Endpoint Verification extension is installed in one or more profiles" -Level "INFO"
+}
+else {
+    Write-Message -Message  "Endpoint Verification extension could not be found in any Chrome profile" -Level "WARN"
+    $Summary += "Endpoint Verification extension is not installed"
+}
+
+#====================================================================================================#
+#                                     [ Firewall Status Check ]                                      #
+#====================================================================================================#
+$FirewallStatus = Get-NetFirewallProfile | Select-Object Name, Enabled
+
+foreach ($NetProfile in $FirewallStatus) {
+    if ($NetProfile.Enabled -eq $false) {
+       Write-Message -Message "The $($NetProfile.Name) firewall profile is disabled" -Level "WARN"
+
+        $EnableResponse = Show-MessageBox -Message "The $($NetProfile.Name) firewall profile is disabled.`n`nWould you like to enable it?" -Title "Enable Firewall Profile?" -Icon "Question" -Buttons ([System.Windows.Forms.MessageBoxButtons]::YesNo)
+        if ($EnableResponse -eq [System.Windows.Forms.DialogResult]::Yes) {
+            try {
+                Set-NetFirewallProfile -Name $NetProfile.Name -Enabled True
+                Write-Message -Message "$($NetProfile.Name) firewall profile has been enabled" -Level "NOTICE" -Dialogue $true
+            }
+            catch {
+                Write-Message -Message "Failed to enable the $($NetProfile.Name) firewall profile`n`n$($_.Exception.Message)" -Level "ERROR" -Dialogue $true
+                exit 1
+            }
+        }
+        else {
+            Write-Message -Message "User chose not to enable the $($NetProfile.Name) firewall profile." -Level "NOTICE"
+            $Summary += "$($NetProfile.Name) firewall profile."
+        }
+    }
+    else {
+        Write-Message -Message "Firewall $($NetProfile.Name) profile is enabled" -Level "INFO"
+    }
+}
+
+#====================================================================================================#
+#                               [ Endpoint Verification Helper Check ]                               #
+#====================================================================================================#
+$EVHelperApp = Get-Package | Where-Object { $_.Name -like "*Google Endpoint Verification*" }
+
+if ($null -eq $EVHelperApp) {
+    Write-Message -Message "Endpoint Verification Helper is not installed" -Level "WARN"
+    
+    $InstallResponse = Show-MessageBox -Message "Endpoint Verification Helper is missing.`n`nWould you like to install it now?" -Title "Install EV Helper?" -Icon "Question" -Buttons ([System.Windows.Forms.MessageBoxButtons]::YesNo)
+    
+    if ($InstallResponse -eq [System.Windows.Forms.DialogResult]::Yes) {
+         Write-Message -Message "Downloading EV Helper File..." -Level "INFO"
+
+        try {
+            Invoke-WebRequest $EVHelperURL -outfile $EVHelperPath
+            Write-Message -Message "Endpoint Verification Helper file downloaded" -Level "NOTICE" 
+        }
+        catch {
+            Write-Message -Message "Failed to download EV Helper file`n`n$($_.Exception.Message)" -Level "ERROR" -Dialogue $true
+            exit 1
+        }
+
+        # Enable Admin account
+        Write-Message -Message "Prompting for builtin administrator credentials..." -Level "INFO"
+        $AdminCred = Get-Credential -UserName "administrator" -Message "Enter or set the local admin credentials."
+
+        Write-Message -Message "Checking if builtin administrator account is enabled..." -Level "INFO"
+        $BuiltinAdmin = Get-LocalUser -Name "Administrator"
+
+        if (!$BuiltinAdmin.Enabled) {
+            Write-Message -Message "Account is disabled. Enabling..." -Level "INFO"
+            try {
+                Enable-LocalUser -Name "Administrator"
+                Write-Message -Message "Builtin administrator account enabled" -Level "NOTICE"
+
+                Set-LocalUser -Name "Administrator" -Password $AdminCred.Password
+                Write-Message -Message "Administrator password updated" -Level "NOTICE"
+
+                $SetByScript = 1
+            }
+            catch {
+                Write-Message -Message "Failed to activate builtin administrator account`n`n$($_.Exception.Message)" -Level "ERROR" -Dialogue $true
+            }
+        }
+        else {
+            Write-Message -Message  "Account is already enabled" -Level "INFO"
+        }
+
+        # Run the MSI installer
+        Write-Message -Message  "Installing Endpoint Verification Helper..." -Level "INFO"
+        try {
+            Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$EVHelperPath`"" -Credential $AdminCred -Wait
+            Write-Message -Message  "Endpoint Verification Helper installed" -Level "NOTICE"        }
+        catch {
+            Write-Message -Message  "Installation failed!`n`n$($_.Exception.Message)" -Level "ERROR" -Dialogue $true
+            exit 1
+        }
+        
+        # Clean up files in Temp folder and remove admin password from memory
+        try {
+            Write-Message -Message  "Deleting .msi file..." -Level "INFO"           
+            Remove-Item $EVHelperPath -Force
+            Write-Message -Message  "MSI file deleted" -Level "NOTICE"
+            Remove-Variable AdminCred
+        }
+        catch {
+            Write-Message -Message  "Cleanup failed: $($_.Exception.Message)" -Level "WARN"
+            continue
+        }
+
+        # Disable Administrator account if enabled by the script
+        if ($SetByScript -eq 1) {
+            Write-Message -Message  "Disabling builtin administrator account..." -Level "INFO"
+            Disable-LocalUser -Name "Administrator"
+            Write-Message -Message  "Builtin administrator account disabled" -Level "NOTICE"
+        }
+
+        Show-MessageBox -Message "Google Endpoint Verification has been installed." -Title "Information" -Icon "Information"
+    }
+    else {
+        Write-Message -Message "User chose not to install Endpoint Verification Helper." -Level "NOTICE"
+        $Summary += "Endpoint Verification Helper is not installed"
+    }
+}
+else {
+    Write-Message -Message  "Endpoint Verification Helper is installed" -Level "INFO"
+}
+
+#====================================================================================================#
+#                                       [ Compliance Summary ]                                       #
+#====================================================================================================#
+if ($Summary.Count -eq 0) {
+    $Message = @"
+    All checks passed. Your device is compliant.
+
+    If you're still unable to access Gmail, try the following:
+
+      1. Open Google Chrome and wait 2 minutes
+      2. Open the Endpoint Verification extension
+      3. Click 'SYNC NOW'
+      4. Reload your Gmail tab
+"@
+    Show-MessageBox -Message $Message -Title "Information" -Icon "Information"
+} 
+else {
+    $SummaryText = ($Summary | ForEach-Object { "    - $_" }) -join "`n"
+    $Message = @"
+    Your device is not compliant:
+
+$SummaryText
+
+Please address each issue and then run the Endpoint Verification sync to regain access.
+"@
+    Show-MessageBox -Message $Message -Title "Information" -Icon "Error"
+}
