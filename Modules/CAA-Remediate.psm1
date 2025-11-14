17045435a698e00755487a5907a91c01e89576ad
HTTP-218 <http218@celenyx.com>
2025-11-14 19:29:34 +0000

    Add missing MSI arguments

--- Modules/CAA-Remediate.psm1
+++ Modules/CAA-Remediate.psm1
@@ -148,10 +148,10 @@ function Install-EVHelperApp {
     }
 
     Write-Message -Message  "Installing Endpoint Verification Helper..." -Level "INFO"
-    $MSIArgs = "/i `"$EVHelperPath`" /qn /norestart"
+    #$MSIArgs = "/i `"$EVHelperPath`" /qn /norestart"
 
     try {
-        $Process = Start-Process -FilePath "msiexec.exe" -ArgumentList $MSIArgs -Credential $AdminCred -Wait -PassThru
+        $Process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$EVHelperPath`" /qn /norestart" -Credential $AdminCred -Wait -PassThru
 
         if ($Process.ExitCode -eq 0) {
             Write-Message -Message "Endpoint Verification Helper installed successfully." -Level "NOTICE" -ForegroundColor Green
